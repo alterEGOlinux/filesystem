bf9e01af255ba98ff89727bff7c9cafc931d19f9
Pascal Malouin <pascal.malouin@gmail.com>
2023-11-09 12:48:14 -0500

    Updated using lazygit...

--- usr/local/bin/terminal-colors.sh
+++ usr/local/bin/terminal-colors.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-
 ## ----------------------------------------------------------------------- INFO
-## [terminal-colors.sh]
-## created     = "2020-11-25 13:14:03 UTC"
-## updated     = "2020-11-25 13:47:35 UTC"
-## description = "Collection of scripts showing terminal colors."
+## [/usr/local/bin/terminal-colors.sh]
+## author        : fantomH @alterEGO Linux
+## created       : 2020-11-25 13:14:03 UTC
+## updated       : 2023-11-09 11:52:40 UTC
+## description   : Collection of scripts showing terminal colors.
 
 stack() {
     ## ref. https://unix.stackexchange.com/a/60969
-    for i in {0..255} ; do
+    for i in {0..255}; do
         printf "\x1b[38;5;${i}mcolour${i}\n"
     done
     }
@@ -28,6 +28,8 @@ usage() {
 Options:
   --stack
   --columns
+  --help, --help
+  --usage
 EOF
     }
 
@@ -41,10 +43,11 @@ case "${@}" in
         columns
         ;;
 
-    '' )
+    ''|-h|--help|--usage )
         usage
         ;;
 
 esac
 
+# vim: foldmethod=marker
 ## ------------------------------------------------------------- FIN ¯\_(ツ)_/¯
