865c596a14fb70f850afb621e472fc142dccdabc
Pascal Malouin <pascal.malouin@gmail.com>
2023-11-16 10:27:31 -0500

    Updated using lazygit...

--- usr/local/share/reverse_shells/rev_ssh.bash
+++ usr/local/share/reverse_shells/rev_ssh.bash
@@ -1,58 +1,31 @@
 #!/usr/bin/env bash
-## ----------------------------------------------------------------------------
-##             { alterEGO Linux: "Open the vault of knowledge" }             ##
-## ----------------------------------------------------------------------------
-##
-## /usr/local/share/reverse_shells/rev_ssh.bash
-##   created        : 2022-01-10 16:23:27 UTC
-##   updated        : 2022-11-03 10:56:23 UTC
-##   description    : Set up a reverse ssh connexion.
-## ____________________________________________________________________________
+## ----------------------------------------------------------------------- INFO
+## [/usr/local/share/reverse_shells/rev_ssh.bash]
+## author        : fantomH @alterEGO Linux
+## created       : 2022-01-10 16:23:27 UTC
+## updated       : 2023-11-16 11:54:13 UTC
+## description   : Set up a reverse ssh connexion.
 
-message() {
+## ( COLORS )
 
-  ## Usage: message <type> <"Message">
-
-  _msg_blue="\033[34m"
-  _msg_bold="\033[1m"
-  _msg_green="\033[32m"
-  _msg_red="\033[31m"
-  _msg_reset="\033[0m"
-
-  _msg="${2}"
-
-  case ${1} in
-
-    action )
-      printf '%b\n' "${_msg_green}[*]${_msg_reset} ${_msg_bold}${_msg}${_msg_reset}"
-      ;;
-    result )
-      printf '%b\n' "${_msg_blue}[-]${_msg_reset} ${_msg_bold}${_msg}${_msg_reset}"
-      ;;
-    question )
-      ## The input will be in $_INPUT.
-      read -p $'\033[34m[?]\033[0m \033[1m'"${_msg}"$'\033[0m ' _INPUT
-      ;;
-    warning|alert )
-      printf '%b\n' "${_msg_red}[!]${_msg_reset} ${_msg_bold}${_msg}${_msg_reset}"
-      ;;
-    title )
-      printf '%b\n' "${_msg_bold}${_msg}${_msg_reset}"
-      ;;
-  esac
-}
+  _BLUE="\033[34m"
+  _BLUELESS=$(printf '%b' ${_BLUE})
+  _BOLD="\033[1m"
+  _BOLDLESS=$(printf ${_BOLD})
+  _GREEN="\033[32m"
+  _GREENLESS=$(printf '%b' ${_GREEN})
+  _RED="\033[31m"
+  _RESET="\033[0m"
+  _RESETLESS=$(printf '%b' ${_RESET})
 
 usage() {
-    _blue=$(printf '%b' "\033[34m")
-    _bold=$(printf '%b' "\033[1m")
-    _reset=$(printf '%b' "\033[0m")
 
-    cat << EOF | less -R
-## ----------------------------------------------------------------------------
+    cat << EOF | more
+## ------------------------------------------------------------------------- ##
 ##             { alterEGO Linux: "Open the vault of knowledge" }             ##
-## ----------------------------------------------------------------------------
+## ------------------------------------------------------------------------- ##
 
-${_bold}USAGE:${_reset} rev_ssh.bash <user@home_address:port>
+${_BOLDLESS}USAGE:${_RESETLESS} rev_ssh.bash <user@home_address:port>
 
 This script creates a ssh reverse tunnel that allows you to bypass the firewall 
 blocking ssh incomming connections on a server or PC.
@@ -68,7 +41,7 @@ On the home machine:
 
   $ ssh -p <port> localhost
 
-## [-- ${_blue}${_bold}How does it work: the basic${_reset} --]
+## [ ${_BLUELESS}${_BOLDLESS}How does it work: the basic${_RESETLESS} ]
 
 SSH reverse tunneling uses local port forwarding on the local machine from an
 already existing ssh connection:
@@ -92,7 +65,7 @@ Usually, a ssh connection will timeout and disconnect from the server if no
 data is received from the server. In order to bypass this, ServerAliveInterval
 option sets automatic message every 60 seconds if no activity occurs.
 
-## [-- ${_blue}${_bold}localhost${_reset} --]
+## [ ${_BLUELESS}${_BOLDLESS}localhost${_RESETLESS} ]
 
 ssh doesn't seem to require localhost to be defined to work, but it doesn't 
 hurt to set the localhost in /etc/hosts like so:
@@ -100,7 +73,7 @@ hurt to set the localhost in /etc/hosts like so:
   127.0.0.1        localhost
   ::1              localhost
 
-## [-- ${_blue}${_bold}Password vs ssh-key${_reset} --]
+## [ ${_BLUELESS}${_BOLDLESS}Password vs ssh-key${_RESETLESS} ]
 
 Using this script with a password will work with limitation.
 
@@ -119,7 +92,7 @@ On the remote server:
 This will create a public and private key on the remote server, and then copy 
 the public key to home.
 
-## [-- ${_blue}${_bold}In the background with tmux${_reset} --]
+## [ ${_BLUELESS}${_BOLDLESS}In the background with tmux${_RESETLESS} ]
 
 Although running rev_ssh.bash by itself is nice, to enable a more permanent
 reverse connection, you can use a multiplexer like tmux to keep the process
@@ -142,7 +115,7 @@ you can access it like so:
 
   $ env TERM=screen-256color tmux -u a -t <session_name>
 
-## [-- ${_blue}${_bold}Further reading${_reset} --]
+## [ ${_BLUELESS}${_BOLDLESS}Further reading${_RESETLESS} ]
 
 • ServerFault - SSH remote port forwarding failed
   https://serverfault.com/questions/595323/ssh-remote-port-forwarding-failed/615751#615751
@@ -159,16 +132,15 @@ you can access it like so:
 • branko, "Bypassing corporate firewall with reverse ssh port forwarding", think shell
   http://toic.org/blog/2009/reverse-ssh-port-forwarding/
 
-## ___________________________{ FIN ¯\_(ツ)_/¯ }_______________________________
+## ------------------------------------------------------------- FIN ¯\_(ツ)_/¯
 EOF
 }
 
 reverse_ssh() {
     ## ref. https://serverfault.com/a/615751
-    message action "Initiating reverse ssh connection..."
-    message result "Use ssh -p ${port} localhost at home..."
-    while true
-    do
+    printf '%b\n' "${_GREEN}[*]${_RESET} ${_BOLD}Initiating reverse ssh connection...${_RESET}"
+    printf '%b\n' "${_BLUE}[-]${_RESET} ${_BOLD}Use ssh -p ${port} localhost at home...${_RESET}"
+    while true; do
         ssh -f ${user}@${home_address} -R ${port}:localhost:22  -N -o ExitOnForwardFailure=yes -o ServerAliveInterval=60
         sleep 180
     done
@@ -187,9 +159,9 @@ case ${@} in
 
 esac
 
-## [-- TODO --]
+## -------------------- [ TODO ]
 
 ## Include option --tmux
 
 # vim: foldmethod=marker
-## ___________________________{ FIN ¯\_(ツ)_/¯ }_______________________________
+## ------------------------------------------------------------- FIN ¯\_(ツ)_/¯
